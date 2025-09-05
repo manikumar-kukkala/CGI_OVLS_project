b46fcf78a762c4ddbefb57be4b6bb355cb37efaa
srushti-collab <srushtihosmane30@gmail.com>
2025-09-05 14:58:26 +0530

    Update UserController.java

--- Backend_main/src/main/java/com/cgi/controller/UserController.java
+++ Backend_main/src/main/java/com/cgi/controller/UserController.java
@@ -14,17 +14,34 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    
-
     // Register user
     @PostMapping("/register")
-    public User registerUser(@RequestBody User user) {
-        return userService.addUser(user);
+    public String registerUser(@RequestBody User user) {
+        return userService.userRegistration(user);
     }
 
-    // Fetch all users (you'll need to add this method in UserServiceImpl)
+    // User login
+    @PostMapping("/login")
+    public String login(@RequestParam String email, @RequestParam String password) {
+        return userService.userLogin(email, password);
+    }
+
+    // Change password
+    @PutMapping("/change-password")
+    public String changePassword(@RequestBody User user) {
+        return userService.changePassword(user);
+    }
+
+    // Forgot password
+    @GetMapping("/forgot-password")
+    public String forgotPassword(@RequestParam String email) {
+        return userService.forgotPassword(email);
+    }
+
+    // Fetch all users
     @GetMapping("/all")
     public List<User> getAllUsers() {
-        return userService.getAllUsers();  // Implement getAllUsers() in service
+        return userService.getAllUsers();
     }
 }
+
